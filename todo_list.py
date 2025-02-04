30b6777dd6b4996ebd5fdc1589c9898097127da5
Krish Tanwar <krishtanwar458@gmail.com>
2025-02-03 22:43:36 -0800

    Add doc-strings to the function definitions

--- todo_list.py
+++ todo_list.py
@@ -1,9 +1,12 @@
-def get_todos(filepath):
+def get_todos(filepath = 'todos.txt'):
+     """ Read a text file and return the list of
+     to-do items. """
      with open(filepath, 'r') as file_local:
                todo_list_local = file_local.readlines()
-     return filepath
+     return todo_list_local
 
-def write_todos(filepath, todo_list_local):
+def write_todos(todo_list_local, filepath = 'todos.txt'):
+     """ Write the to-do items list in a text file. """
      with open(filepath, 'w') as file:
                file.writelines(todo_list_local)
 
@@ -14,12 +17,12 @@ while True:
 
      if action.startswith('add'):
           user_input1 = action[4:] + '\n'              # list slicing - user_input1 = action[4:]
-          todo_list = get_todos('todos.txt')
+          todo_list = get_todos()
           todo_list.append(user_input1.capitalize())
-          write_todos('todos.txt', todo_list)
+          write_todos(todo_list)
 
      elif action.startswith('show'):
-          todo_list = get_todos('todos.txt')
+          todo_list = get_todos()
           for index, i in enumerate(todo_list):
                i = i.strip()
                print(f'{index + 1}. {i}')
@@ -31,7 +34,7 @@ while True:
      
      elif action.startswith('edit'):
           try:
-               todo_list = get_todos('todos.txt')
+               todo_list = get_todos()
                action2 = action.strip('edit ')
                for i in todo_list:
                     number = int(action2) 
@@ -43,7 +46,7 @@ while True:
                     todo_list[number] = f'{action3.capitalize() + '\n'}' 
                     changed_todo = todo_list[number]
                     print(f'{todo_to_change.strip('\n')} has been changed to {changed_todo}')
-                    write_todos('todos.txt', todo_list)
+                    write_todos(todo_list)
                     break
           except ValueError:
                print('Your command is invalid!')
@@ -51,17 +54,19 @@ while True:
 
      elif action.startswith('complete'):
           try:
-               todo_list = get_todos('todos.txt')
+               todo_list = get_todos()
                action4 = action.strip('complete ')
                action4 = action4.strip()
                action4 = int(action4) - 1
                completed_todo = todo_list[action4].strip('\n')
                todo_list.pop(action4)
-               write_todos('todos.txt', todo_list)
+               write_todos(todo_list)
                print(f'{completed_todo} marked as completed')
           except IndexError:
                print('You can only complete available todos!')
                continue
+          except ValueError:
+                print("You must enter a number after 'complete' command!")
 
      elif action.startswith('exit'):
           print('Bye!')
