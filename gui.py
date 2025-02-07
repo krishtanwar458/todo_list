1e47fff879dafd6ffdf086950fb71bb1223b8aa4
Krish Tanwar <krishtanwar458@gmail.com>
2025-02-07 14:37:08 -0800

    Added a working 'Edit' function

--- gui.py
+++ gui.py
@@ -20,8 +20,20 @@ while True:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
-            todos.append(new_todo)
+            todos.append(new_todo.capitalize())
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo.capitalize() + '\n'
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
