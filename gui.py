0679d0ae27fb26e912622cca896355a7fb4b7197
Krish Tanwar <krishtanwar458@gmail.com>
2025-02-07 13:16:13 -0800

    added list_box and an edit_button

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import FreeSimpleGUI as sg
 label = sg.Text('Type in a todo.')
 input_box = sg.InputText(tooltip='Enter todo', key='todo')
 add_button = sg.Button('Add')
+list_box = sg.Listbox(values = functions.get_todos(), key='todos', 
+                      enable_events=True, size=[45,10])
+edit_button = sg.Button('Edit')
 
 window = sg.Window('Todo App', 
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Calibri', 12))
 
 while True:
