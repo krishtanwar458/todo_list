879c285c52b56dba804eb8e38c96d089c1e29cbb
Krish Tanwar <krishtanwar458@gmail.com>
2025-02-08 18:55:34 -0800

    Made a working file zipping program

--- File_Zipper.py
+++ File_Zipper.py
@@ -1,19 +1,29 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text('Select the files to compress')
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse('Choose')
+choose_button1 = sg.FilesBrowse('Choose', key='files')
 
 label2 = sg.Text('Select the destination folder')
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse('Choose')
+choose_button2 = sg.FolderBrowse('Choose', key='folder')
+output_label = sg.Text(key='output')
 
 compress_button = sg.Button('Compress')
 
 window = sg.Window('File Zipper', 
                    layout = [[label1, input1, choose_button1], 
                              [label2, input2, choose_button2], 
-                             [compress_button]])
-window.read()
-window.close()
+                             [compress_button, output_label]])
 
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values['files'].split(';')
+    folder = values['folder']
+    make_archive(filepaths, folder)
+    window['output'].update(value='Compression completed!')
+    break
+
+window.close()
