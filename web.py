b5efcd5297cb816e088feefe9b2ea6d26bbab711
Krish Tanwar <krishtanwar458@gmail.com>
2025-02-15 18:49:12 -0800

    Implement add new todo items feature on webpage

--- web.py
+++ web.py
@@ -2,11 +2,19 @@ import streamlit as st  #/Library/Frameworks/Python.framework/Versions/3.13/bin/
 import functions
 
 todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state['new_todo']
+    todos.append(todo.capitalize() + '\n')
+    functions.write_todos(todos)
+
+todos = functions.get_todos()
 
 st.title('Todo App')
 
 for index, todo in enumerate(todos):
     st.checkbox(todo, key=f'todo_{index}')
 
-st.text_input(label='Enter a todo:', placeholder='Add a new todo')
+st.text_input(label='Enter a todo:', placeholder='Add a new todo', on_change=add_todo, 
+              key='new_todo')
 
+st.session_state
