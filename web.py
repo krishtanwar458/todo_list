b0e87a9b98bfba29f4edbc14854d834e1fd518e9
Krish Tanwar <krishtanwar458@gmail.com>
2025-02-15 19:07:04 -0800

    Added a working complete feature to the webpage

--- web.py
+++ web.py
@@ -12,9 +12,12 @@ todos = functions.get_todos()
 st.title('Todo App')
 
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f'todo_{index}')
+    checkbox = st.checkbox(todo, key=f'{todo}{index+1}')
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[f'{todo}{index+1}']
+        st.rerun()
 
 st.text_input(label='Enter a todo:', placeholder='Add a new todo', on_change=add_todo, 
               key='new_todo')
-
-st.session_state
